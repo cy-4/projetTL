3571f0974a717cb3d41470e28a0b2262ca3144ef
Arnold Cyprien <carnold@azote.enseeiht.fr>
2022-01-11 14:20:24 +0100

    rajout commentaire

--- passePlacementRat.ml
+++ passePlacementRat.ml
@@ -11,59 +11,61 @@ struct
   type t2 = Ast.AstPlacement.programme
 
 
-(* analyse_tds_instruction : AstSyntax.instruction -> tds -> AstTds.instruction *)
-(* Paramètre tds : la table des symboles courante *)
+(* analyse_placement_instruction : AstType.instruction  *)
 (* Paramètre i : l'instruction à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme l'instruction
-en une instruction de type AstTds.instruction *)
-(* Erreur si mauvaise utilisation des identifiants *)
+(* Paramètre base : à combien de registre l'instruction se trouve *)
+(* Paramètre reg : savoir si c'est SB ou LB *)
+(* Met dans les infos des variables leur placement mémoire et renvoie la taille que prend l'instruction *)
 let rec analyse_placement_instruction i base reg =
   match i with
   | AstType.Declaration (info, _) ->
       begin
       match info_ast_to_info info with
         | InfoVar(_,t,_,_) -> let taille = getTaille t in 
+                             (* On rajoute l'information du placement mémoire dans la variable*)
                              modifier_adresse_info base reg info;
                              taille
         | _ -> failwith "Internal Error"
       end
 
   | AstType.Conditionnelle (_,t,e) -> 
+    (*On rajoute l'information du placement mémoire au variable des 2 blocs*)
     let _ = analyse_placement_bloc t base reg in
     let _ = analyse_placement_bloc e base reg in
+    (* A la fin de la Conditionnelle, on a pas augmenté le registre courant*)
     0
 
   | AstType.TantQue (_,b) -> 
+      (* On rajoute l'information du placement mémoire au variable du bloc*)
       let _ = analyse_placement_bloc b base reg in
+      (* A la fin du while, on a pas augmenté le registre courant*)
       0
-
    | _ -> 0
  
 
       
-(* analyse_tds_bloc : AstSyntax.bloc -> AstTds.bloc *)
-(* Paramètre tds : la table des symboles courante *)
+(* analyse_placement_bloc : AstTds.bloc *)
+(* Paramètre base : à combien de registre l'instruction se trouve *)
+(* Paramètre reg : savoir si c'est SB ou LB *)
 (* Paramètre li : liste d'instructions à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme le bloc
-en un bloc de type AstTds.bloc *)
-(* Erreur si mauvaise utilisation des identifiants *)
+(* On rajoute le placement mémoire à chaque instruction du bloc *)
 and analyse_placement_bloc li base reg =
    begin
    match li with
         | [] -> base
-
+        (* On rajoute l'information du placement mémoire à l'instrution,
+        Puis on fait appel à nouveaux à la fonction suivate avec la base m-a-j*)
         | t::q -> let taille = analyse_placement_instruction t base reg in
                                 analyse_placement_bloc q (base+taille) reg
     end
 
 
 
-(* analyse_tds_fonctiobaseilisation des identifiants et tranforme la fonction
-en une fonction de type AstTds.fonction *)
-(* Erreur si mauvaise utilisation des identifiants *)
-  
+(* analyse_placement_fonction : AstTds.fonction *)
+(* Paramètre fonction : fonction dont qu'on modifie *)
+(* On rajoute le placement mémoire à chaque paramètre de la fonction et aux instructions de la fonction *) 
 let analyse_placement_fonction _  (AstType.Fonction(info,lp,li)) =
-    
+    (* On rajoute l'information du placement mémoire aux arguments de la fonction*)
     let _ = List.fold_right (
       fun pointeur reg ->
         match info_ast_to_info pointeur with
@@ -73,18 +75,19 @@ let analyse_placement_fonction _  (AstType.Fonction(info,lp,li)) =
           | _ -> failwith "Interne error"
     )
      lp 0 in
+     (*  On rajoute l'information du placement mémoire aux instructions de la fonctions*)
      let _ = analyse_placement_bloc li 3 "LB" in
      (AstPlacement.Fonction(info,lp,li)
     )
                                             
-(* analyser : AstSyntax.ast -> AstTds.ast *)
-(* Paramètre : le programme à analyser *)
-(* Vérifie la bonne utilisation des identifiants et tranforme le programme
-en un programme de type AstTds.ast *)
-(* Erreur si mauvaise utilisation des identifiants *)
 
+(* analyser : AstType.ast -> AstType.ast *)
+(* Paramètre : le programme à analyser *)
+(* Ajoute l'information du placement mémoire à chaque vériable du programme *)
 let analyser (AstType.Programme (fonctions,prog)) =
+  (* On joute l'information du placement mémoire aux fonctions*)
   let nf = List.map (analyse_placement_fonction 0) fonctions in
+  (* On joute l'information du placement mémoire au main*)
   let _ = analyse_placement_bloc prog 0 "SB" in
   AstPlacement.Programme(nf,prog)
 
